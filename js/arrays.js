c0480af04dedd14447e9fdad1f65dc500101073d
Gonzalo Mirasierra <gonzalo.mirasierra@gmail.com>
2023-05-31 23:06:04 +0200

    Subida de varias cosas

---
+++ js/arrays.js
@@ -0,0 +1,15 @@
+const lista = [1,2,3,4]
+
+const arr = [1, 'hola', true, {}]
+
+const arr2d = [
+    [1,2,3],
+    [4,5,6]
+]
+
+lista[0] // 1
+
+lista[lista.length() -1] // 4
+
+arr2d[0] // [1,2,3]
+arr2d[0][0] // 1
---
+++ js/objetos.js
@@ -0,0 +1,15 @@
+const miObjeto = {
+    nombre: 'Gonzalo',
+    edad: 31,
+    conPerro: true,
+    ordenadores: [
+        'ASUS',
+        'nanai'
+    ]
+}
+
+miObjeto.nombre // Gonzalo
+miObjeto.edad // 31
+miObjeto.ordenadores // Los ordenadores que he puesto
+
+miObjeto.extra = 'Sí'
